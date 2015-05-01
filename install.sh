6eb3b99ea7b975b7869c8df7ee26538944bf382e
cryptaxe <cryptaxe@rat>
2015-05-01 11:24:58 -0700

    fixed

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "runtime vimrc" > ~/.vimrc && mkdir ~/.vim && cd ~/.vim &&  cp .tmux.conf ~/ && git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim && vim +PluginInstall
+echo "runtime vimrc" > ~/.vimrc && mkdir ~/.vim && cd ~/.vim && git clone https://github.com/cryptaxe/vimrc.git . &&  cp .tmux.conf ~/ && git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim && vim +PluginInstall
